1b565b7617e401f7f40c23805d9e64f9bf297b08
kenz567 <mekenzie-cooper@proton.me>
2026-07-08 11:53:45 +0100

    Change static file serving to main directory
    Updated server to serve index.html from the main folder instead of a public folder.

--- server.js
+++ server.js
@@ -7,20 +7,18 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const path = require('path');
 
-// 1. Tell the server to look in the 'public' folder for files (CSS, JS, HTML)
-app.use(express.static(path.join(__dirname, 'public')));
+// 1. Tell the server to look in the MAIN folder (not a public folder)
+app.use(express.static(__dirname));
 
-// 2. The "Home" route: When someone visits your URL, send them the index.html
+// 2. When someone visits the home URL, send them index.html from the main folder
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// 3. The "Brain" (Socket.io): This handles real-time messaging
+// 3. The "Brain" (Socket.io)
 io.on('connection', (socket) => {
   console.log('A friend connected!');
 
-  // When the server gets a 'chat message' from one person,
-  // it immediately sends it to EVERYONE connected (io.emit)
   socket.on('chat message', (data) => {
     io.emit('chat message', data);
   });

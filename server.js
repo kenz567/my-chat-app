482c04492d9eb8b959d567e36257b67e774e2dfc
kenz567 <mekenzie-cooper@proton.me>
2026-07-09 10:58:09 +0100

    Implement custom profanity filter for chat messages
    Added a custom profanity filter to clean chat messages.

--- server.js
+++ server.js
@@ -7,26 +7,36 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const path = require('path');
 
-// 1. Load the "bad-words" filter robot
-const Filter = require('bad-words');
-const filter = new Filter();
-
-// 2. Tell the server to look in the MAIN folder for files
+// 1. Tell the server to look in the MAIN folder for files
 app.use(express.static(__dirname));
 
-// 3. The "Home" route
+// 2. The "Home" route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// 4. The "Brain" (Socket.io)
+// --- PROFANITY FILTER ---
+// ADD YOUR WORDS HERE:
+const badWords = ['fuck', 'shit', 'idiot', 'bitch', 'kys', 'kill', 'kill urself', 'kill yourself', 'test words btw', '']; 
+
+function filterMessage(text) {
+  let cleanedText = text;
+  badWords.forEach(word => {
+    // 'gi' makes it case-insensitive (catches 'BadWord' and 'badword')
+    const regex = new RegExp(word, 'gi'); 
+    cleanedText = cleanedText.replace(regex, '***');
+  });
+  return cleanedText;
+}
+// ------------------------
+
+// 3. The "Brain" (Socket.io)
 io.on('connection', (socket) => {
   console.log('A friend connected!');
 
   socket.on('chat message', (data) => {
-    // AUTOMATIC FILTERING:
-    // This cleans the text instantly!
-    const cleanText = filter.clean(data.text);
+    // Apply the filter before broadcasting the message
+    const cleanText = filterMessage(data.text);
     
     // Broadcast the cleaned message to everyone
     io.emit('chat message', { user: data.user, text: cleanText });
@@ -37,7 +47,7 @@ io.on('connection', (socket) => {
   });
 });
 
-// 5. Start the server
+// 4. Start the server
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
